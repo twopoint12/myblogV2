4e79b3eca4cf032a41f6e8ac8ca41b6cdf2df3e6
Xiexuan <abc.jack@126.com>
2020-05-25 01:35:25 +0800

    update production setting

--- myblog2/settings/prod.py
+++ myblog2/settings/prod.py
@@ -1,7 +1,7 @@
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mjrwao7(+qbn#w&g+x=9ymbq!2ve-2tup)=ihoc57e*!v(ip^6'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
